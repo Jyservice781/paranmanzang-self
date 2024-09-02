e69ee42b2e7bed2615b973ce909ee3dfb164febb
Jyservice781 <gimjuyeong512@gmail.com>
2024-09-02 17:49:37 +0900

    second try

--- src/main/java/com/category/paranmanzang/model/domain/HotelModel.java
+++ src/main/java/com/category/paranmanzang/model/domain/HotelModel.java
@@ -1,27 +1,12 @@
 package com.category.paranmanzang.model.domain;
 
-import lombok.Builder;
 import lombok.Data;
 import org.springframework.stereotype.Component;
-
-import java.util.Date;
 
 @Component
 @Data
 public class HotelModel {
     public Long id;
     public String name;
-    public String address;
-    public Date startEntry;
-    public Date endEntry;
-    public boolean booked;
-    public double price;
-    public int sellerId;
-    public String content;
-    public String shortContent;
-    public int roomNumber;
-    public int roomType;
-    public int roomMember;
-    public String nickname;
 
 }
